18ee8d6518100bbe1659f36540ec72a58a33559d
Anton Krizmanic <anton.krizmanic94@gmail.com>
2022-05-12 18:06:51 +0200

    Section 5

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,44 @@
-import { getFeaturedEvents } from '../dummy-data';
-import EventList from '../components/events/event-list';
+import path from 'path';
+import fs from 'fs/promises';
 
-export default function HomePage() {
+import Link from 'next/link';
 
-    const featuredEvents = getFeaturedEvents();
+function HomePage(props) {
+    const { products } = props;
     return (
-        <div>
-            <EventList items={featuredEvents} />
-        </div>
+        <ul>
+            {products.map(product =>
+                <li key={product.id}><Link href={`/products/${product.id}`}>{product.title}</Link></li>
+            )}
+        </ul>
     )
 }
+
+export async function getStaticProps(context) {
+    // process.cwd nas stavlja u root folder
+    const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+
+    const jsonData = await fs.readFile(filePath)
+    const data = JSON.parse(jsonData);
+
+    if (!data) {
+        return {
+            redirect: ''
+        };
+    }
+
+    if (data.products.length === 0) {
+        return {
+            notFound: true
+        }
+    }
+
+    return {
+        props: {
+            products: data.products
+        },
+        revalidate: 120 //seconds, ignored in development
+    };
+}
+
+export default HomePage;
