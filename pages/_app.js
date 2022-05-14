e4ec9bd1ef044eb2dfade861a665b8badcbe86ca
Anton Krizmanic <anton.krizmanic94@gmail.com>
2022-05-14 06:29:27 +0200

    finish section 8

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,9 @@
 import '../styles/globals.css'
-import CustomLayout from '../components/layout/layout';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <CustomLayout>
-        <Component {...pageProps} />
-      </CustomLayout>
+      <Component {...pageProps} />
     </>
   );
 }
