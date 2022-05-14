b0d001e7528a9a9caf921194d64dc89d9e7eb558
Anton Krizmanic <anton.krizmanic94@gmail.com>
2022-05-14 17:51:46 +0200

    section 9

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,34 @@
 import Head from 'next/head';
+
 import { getFeaturedEvents } from '../helpers/api-util';
 import EventList from '../components/events/event-list';
+import NewsletterRegistration from '../components/input/newsletter-registration';
 
-export default function HomePage(props) {
-    return (
-        <div>
-            <Head>
-                <title>Featured Events</title>
-                <meta name="description" content="Find a lot of events" />
-            </Head>
-            <EventList items={props.events} />
-        </div>
-    )
+function HomePage(props) {
+  return (
+    <div>
+      <Head>
+        <title>NextJS Events</title>
+        <meta
+          name='description'
+          content='Find a lot of great events that allow you to evolve...'
+        />
+      </Head>
+      <NewsletterRegistration />
+      <EventList items={props.events} />
+    </div>
+  );
 }
 
 export async function getStaticProps() {
-    const featuredEvents = await getFeaturedEvents();
+  const featuredEvents = await getFeaturedEvents();
 
-    return {
-        props: {
-            events: featuredEvents
-        },
-        revalidate: 1800
-    }
+  return {
+    props: {
+      events: featuredEvents,
+    },
+    revalidate: 1800,
+  };
 }
+
+export default HomePage;
