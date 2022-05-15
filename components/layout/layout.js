ab8d952f15a8177a11ed56bc6c59486230e59ee7
Anton Krizmanic <anton.krizmanic94@gmail.com>
2022-05-15 07:34:52 +0200

    section 10 finished

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,10 +1,24 @@
+import { useContext } from 'react';
 import MainHeader from './main-header';
+import Notification from '../notification/notification';
+import NotificationContext from '../../store/notification-context';
 
 function CustomLayout(props) {
+  const notificationCtx = useContext(NotificationContext);
+
+  const activeNotification = notificationCtx.notification;
+
   return (
     <>
       <MainHeader />
       <main>{props.children}</main>
+      {activeNotification && (
+        <Notification
+          title={activeNotification.title}
+          message={activeNotification.message}
+          status={activeNotification.status}
+        />
+      )}
     </>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,19 @@
 import Head from 'next/head';
 import '../styles/globals.css'
 import CustomLayout from '../components/layout/layout';
+import { NotificationContextProvider } from '../store/notification-context';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <CustomLayout>
-        <Head>
-          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        </Head>
-        <Component {...pageProps} />
-      </CustomLayout>
+      <NotificationContextProvider>
+        <CustomLayout>
+          <Head>
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          </Head>
+          <Component {...pageProps} />
+        </CustomLayout>
+      </NotificationContextProvider>
     </>
   );
 }
