bcef232c5db46f0f237640ac969d19d22c2b8073
Anton Krizmanic <anton.krizmanic94@gmail.com>
2022-05-19 21:23:42 +0200

    finished section 13

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -21,10 +21,22 @@ function UserProfile() {
   //   return <p className={classes.profile}>Loading...</p>
   // }
 
+  async function changePasswordhandler(passwordData) {
+    const response = await fetch('/api/user/change-password', {
+      method: 'PATCH',
+      body: JSON.stringify(passwordData),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    const data = await response.json();
+  }
+
   return (
     <section className={classes.profile}>
       <h1>Your User Profile</h1>
-      <ProfileForm />
+      <ProfileForm onChangePassword={changePasswordhandler} />
     </section>
   );
 }
---
+++ pages/api/user/change-password.js
@@ -0,0 +1,51 @@
+import { getSession } from 'next-auth/client';
+import { verifyPassword, hashPassword } from '../../../lib/auth';
+import { connectToDatabase } from '../../../lib/db';
+
+async function handler(req, res) {
+    if (req.method !== 'PATCH') {
+        return;
+    }
+    const session = await getSession({ req: req });
+    if (!session) {
+        res.status(401).json({ message: 'Not authenticated!' });
+        return;
+    }
+
+    const userEmail = session.user.email;
+    const oldPassword = req.body.oldPassword;
+    const newPassword = req.body.newPassword;
+
+    const client = await connectToDatabase()
+
+    const usersCollection = client.db().collection('users');
+
+    const user = await usersCollection.findOne({ email: credentials.email });
+    if (!user) {
+        client.close();
+        res.status(404).json({ message: 'User not found' });
+        return;
+    }
+
+    const currentPassword = user.password;
+
+    const passwordsAreEqual = await verifyPassword(oldPassword, currentPassword);
+
+    if (!passwordsAreEqual) {
+        client.close();
+        res.status(403).json({ message: 'Invalid password!' })
+        return;
+    }
+
+    const hashedPassword = await hashPassword(newPassword);
+    const result = usersCollection.updateOne(
+        { email: userEmail },
+        { $set: { password: hashedPassword } }
+    );
+
+    client.close();
+    res.status(200).json({ message: 'Password updated' });
+    return;
+}
+
+export default handler;
